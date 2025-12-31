828c79496652cafca4357de25d73ad512cb66145
Jaydip Sanghani <91427591+jellydeck@users.noreply.github.com>
2025-12-31 10:59:39 +0530

    fix: closing <a> tag

--- src/lib/components/LatestContent.svelte
+++ src/lib/components/LatestContent.svelte
@@ -3,6 +3,7 @@
 	const title = 'Latest Content';
 	const readMoreText = 'Read more';
 </script>
+
 {#if rootContent && rootContent.length > 0}
 	<div class="max-w-6xl mx-auto my-18">
 		<div class="text-center mb-10">
@@ -13,19 +14,21 @@
 		<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
 			{#each rootContent as page}
 				<div class="relative bg-card rounded-xl p-8 transition-all duration-300">
-
+					<a
 						href={page.url}
 						class="inline-flex items-center text-foreground font-medium transition-all"
 					>
 						<div class="relative">
 							<div class="size-6 rounded-lg flex items-center justify-center mb-4">
-								<!-- Add your icon SVG here -->
 								<svg
 									xmlns="http://www.w3.org/2000/svg"
 									class="size-6 text-muted"
 									viewBox="0 0 24 24"
 								>
-									<path fill="currentColor" d="M14 2H6c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V8l-6-6z"/>
+									<path
+										fill="currentColor"
+										d="M14 2H6c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V8l-6-6z"
+									/>
 								</svg>
 							</div>
 							<h3 class="mb-4">{page.metadata.title}</h3>
